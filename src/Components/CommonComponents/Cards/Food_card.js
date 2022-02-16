8fa41af2cabbc97b6a51aceeb8e5babc3cd80956
subhadipbiswas95 <subha.biswas1995@gmail.com>
2022-02-16 23:57:07 +0530

    card update

--- src/Components/CommonComponents/Cards/Food_card.js
+++ src/Components/CommonComponents/Cards/Food_card.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 
 const Food_card = () => {
+  const [users,setUsers]=useState([])
   const getUsers = async () => {
     const response = await fetch("http://localhost:3000/asset/data.json");
-    const data = await response.json();
+    setUsers (await response.json());
 
     //  const data = await response.json();
     // console.log(data);
@@ -11,20 +12,32 @@ const Food_card = () => {
 
   useEffect(() => {
     getUsers();
-  });
+  },[]);
   return (
-    <div className="card" style={{width: '18rem'}}>
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+   <>
+   <div className="row text-center">
+   {
+     users.map((elem)=>{
+       return(
+        <div className="card" style={{width: '18rem' }}>
+        <img src={elem.CardImgSrc} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{elem.ItemName}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+  
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-      </ul>
+       )
+     })
+   }
+   </div>
+   </>
 
-    </div>
+   
   );
 };
 
